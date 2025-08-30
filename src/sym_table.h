e6419ef787dfcd820aa32703d0f7451ff3c708e3
Husain206 <husainal518@gmail.com>
2025-08-30 07:33:06 +0300

    symbol table

--- src/sym_table.h
+++ src/sym_table.h
@@ -3,37 +3,7 @@
 #include <unordered_map>
 #include <vector>
 #include <optional>
-
-using namespace std;
-
-typedef enum {
-  Int,
-  Str,
-  Bool,
-  Nil,
-  Func,
-} Type;
-
-struct Node;
-
-struct Value {
-   Type type = Type::Nil;
-   long long i = 0;
-   string s;
-   bool b = false;
-
-   vector<string> params;
-   const Node* body = nullptr;
-
-   static Value Int(long long v);
-   static Value String(string v);
-   static Value Bool(bool v);
-   static Value Nil();
-   static Value Function(vector<string> ps, const Node* body);
-
-   string toString() const;
-   bool truthy() const;
-};
+#include "dy_types.h"
 
 struct Env {
   vector<unordered_map<string, Value>> scopes;
