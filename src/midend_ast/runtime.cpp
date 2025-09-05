17d74e483972cfd2fe1102e7d9c12ac1fde4043f
Yui <yui300127@gmail.com>
2025-09-05 08:36:29 +0300

    Add brint builtin function

--- src/midend_ast/runtime.cpp
+++ src/midend_ast/runtime.cpp
@@ -5,6 +5,12 @@
 #include <string>
 #include <vector>
 
+Value brint(vector<Value> args) {
+  for(auto arg : args)
+    cout << arg.toString() << " ";
+  cout << endl;
+  return Value::Nil();
+}
 
 void Interpreter::run(const Node* root){
   if(!root || root->type != n_block) throw runtime_error("program must be a block");
@@ -12,6 +18,7 @@ void Interpreter::run(const Node* root){
     // define built-ins
     env.define("true",  Value::Bool(true));
     env.define("false", Value::Bool(false));
+    env.define("brint", Value::NativeFunction(brint));
   exec_block(root);
 }
 
